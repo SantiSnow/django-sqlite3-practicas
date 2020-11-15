0888d61d00603d584ec5cfb0781655e534dac2cf
Santiago <aguirresantiago81@gmail.com>
2020-11-15 17:22:57 -0300

    Cambios en los modelos, eliminados metodos innecesarios,creada una migracion y la bbdd de pruebas

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -10,12 +10,6 @@ class Articulo(models.Model):
     stock = models.IntegerField()
     seccion = models.CharField(max_length=50)
 
-    def __init__(self, nombre, precio, stock, seccion):
-        self.nombre = nombre
-        self.precio = precio
-        self.stock = stock
-        self.seccion = seccion
-
 
 class Cliente(models.Model):
 
@@ -26,14 +20,6 @@ class Cliente(models.Model):
     compras = models.IntegerField()
     comentarios = models.CharField(max_length=120)
 
-    def __init__(self, nombre, direccion, email, telefono, compras, comentarios):
-        self.nombre = nombre
-        self.direccion = direccion
-        self.email = email
-        self.telefono = telefono
-        self.compras = compras
-        self.comentarios = comentarios
-
 
 class Pedido(models.Model):
 
@@ -42,11 +28,4 @@ class Pedido(models.Model):
     entregado = models.BooleanField()
     pagado = models.BooleanField()
 
-    def __init__(self, cantidad, fecha, entregado, pagado):
-        self.cantidad = cantidad
-        self.fecha = fecha
-        self.entregado = entregado
-        self.pagado = pagado
-
-
-
+# no es necesario agregar los constructores ya que django se encarga de esto
