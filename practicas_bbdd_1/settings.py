28201169e541f947c0d08fd4449ddc5b479ae513
Santiago <aguirresantiago81@gmail.com>
2020-11-20 21:06:41 -0300

    Configurado el proyecto para conectar con postgresql

--- practicas_bbdd_1/settings.py
+++ practicas_bbdd_1/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'practicas_bbdd_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pruebas_django',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'DATABASE_PORT': 5432,
     }
 }
 
